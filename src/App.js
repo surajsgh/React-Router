e662254b21b3a2e0fcd9f8a8070d4be3fafc7d17
Suraj Singh <surajsingh220698@outlook.com>
2021-06-10 13:23:50 +0530

    Redirect the user back to the welcome page from the home page.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
@@ -11,6 +11,9 @@ function App() {
       <MainHeader />
       <main>
         <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
           <Route path="/welcome">
             <Welcome />
           </Route>
